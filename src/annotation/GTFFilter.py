0361b21693296fc3e7594abbdc926545695774b7
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2016-03-04 17:06:06 -0600

    Adding annotation and updating comments.

---
+++ src/annotation/GTFFilter.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+# Matthew A. Wyczalkowski, mwyczalk@genome.wustl.edu
+#
+# Simple script to read GTF file line by line, test if criteria are met, and either print or
+# discard line.  Criteria for genes and exons, based on GTF columns:
+# Genes: feature = "gene" and source = "protein_coding"
+# Exons: feature = "exon" and source = "protein_coding"
+#
+# In both cases we require the column "source" of the GTF to be protein_coding.  The contents of
+# this column (in Ensembl 75 at least) are is inconsistent with GTF documentation.  This is discussed
+# here: https://www.biostars.org/p/120306/ Our filtering works for Ensembl 75 GTF but may change in the future.  
+
+import sys
+
+class GTFfeature:
+    """Processes one line of GTF file.  Expands attributes column into key/value pairs"""
+    # GTF fields described in README.ensembl and http://www.ensembl.org/info/website/upload/gff.html
+    def __init__(self, GTFline):
+        self.GTF = GTFline
+        self.seqname, self.source, self.feature, start, end, self.score, self.strand, self.frame, attributes = GTFline.split("\t")
+        self.start, self.end = int(start), int(end)
+        # Split the attributes field.  Example: 'gene_id "ENSG00000172717"; gene_name "FAM71D"; gene_source "ensembl_havana"; gene_biotype "protein_coding";'
+        #   becomes ['gene_id ENSG00000172717', ' gene_name FAM71D', ' gene_source ensembl_havana', ' gene_biotype protein_coding']
+        attr_list = filter(None, attributes.rstrip().replace('"', '').split(";"))
+        # now create key/value dictionary which is saved with this object
+        self.attributes = dict(tok.lstrip().split(' ') for tok in attr_list)
+
+    def asGTF(self):
+        return self.GTF
+
+    def asBED(self, mergeStrand):
+        """Return contents in 6-column BED format: exon, start, end, gene, score, strand
+           Start is 0-based
+           if mergeStrand, add ":+" or ":-" to gene name 
+           """
+        gene = self.attributes["gene_name"]
+        if mergeStrand: gene += ":"+self.strand
+        return "\t".join([self.seqname, str(self.start-1), str(self.end), gene, self.score, self.strand, '\n'])
+
+class GTFexon(GTFfeature):
+    def isFeature(self):    # logically, isExon()
+        """We consider a line an exon if 1) feature = exon and 2) source = protein_coding"""
+        #if self.feature == "exon" and self.attributes['gene_biotype'] == "protein_coding": return True
+        if self.feature == "exon" and self.source == "protein_coding": return True
+        return False
+
+class GTFgene(GTFfeature):
+    def isFeature(self):  # logically, isGene()
+        """We consider a line a gene if 1) feature = gene and 2) source = protein_coding"""
+        #if self.feature == "gene" and self.attributes['gene_biotype'] == "protein_coding": return True
+        if self.feature == "gene" and self.source == "protein_coding": return True
+        return False
+
+def filterGTF(f, o, GTFclass, asBED, mergeStrand):
+    """Go through GTF file line by line and evaluate - using filter method of class specified by the calling function - whether to 
+       retain this line or not.  If line retained, output it in either GTF or BED format"""
+    for l in f:
+        if l.startswith("#"): continue 
+        gtf = GTFclass(l)
+        if gtf.isFeature():   # true for either genes or exons, depending on what class we were passed.
+            if asBED: o.write(gtf.asBED(mergeStrand))
+            else: o.write(gtf.asGTF())
+
+def main():
+    from optparse import OptionParser
+    usage_text = """usage: %prog [options] feature ...
+        Read an ENSEMBL GTF file and retain select features, writing output in GTF or BED format.
+        'feature' is either 'gene' or 'exon'.  This corresponds to GTF 'feature' column of that name and 'gene_biotype' attribute of 'protein_coding'
+        """
+
+    parser = OptionParser(usage_text, version="$Revision: 1.0 $")
+    parser.add_option("-v", dest="verbose", action="store_true", help="verbose output")
+    parser.add_option("-f", dest="in_fn", default="stdin", help="GTF filename")
+    parser.add_option("-o", dest="out_fn", default="stdout", help="Output filename")
+    parser.add_option("-b", dest="as_bed", action="store_true", help="Output in BED format")
+    parser.add_option("-s", dest="mergeStrand", action="store_true", help="Add :-delimited strand information to gene name")
+
+    (options, params) = parser.parse_args()
+    if options.verbose:
+        sys.stderr.write(str(options)+'\n')
+        sys.stderr.write(str(params)+'\n')
+
+    if len(params) != 1:
+        parser.error("Please pass feature name (gene or exon).")
+
+    feature = params[0]
+    if feature not in ["gene", "exon"]:
+        parser.error("Feature must be 'gene' or 'exon'.")
+
+    if options.in_fn == "stdin":
+        f = sys.stdin 
+    else:
+        f = open(options.in_fn, 'r')
+
+    if options.out_fn == "stdout":
+        o = sys.stdout
+    else:
+        o = open(outfn, "w")
+
+    if feature == "gene":
+        filterClass = GTFgene
+    else:
+        filterClass = GTFexon
+        
+    filterGTF(f, o, filterClass, options.as_bed, options.mergeStrand)
+
+    o.close()
+
+if __name__ == '__main__':
+    main()
---
+++ src/annotation/TLAExamine.R
@@ -0,0 +1,141 @@
+# Matthew Wyczalkowski
+# mwyczalk@genome.wustl.edu
+# The Genome Institute
+#
+# Usage: Rscript TLAExamine.R [-v] [-m mode] [-f dat.fn] [-o out.fn] 
+# Version 2.0 (4-27-15)
+
+# Processes GTF and VCF files by expanding a column of key/value pairs into multiple columns with value field
+# Values with repeated keys are separated by ';'
+# -f dat.fn: an ENSEMBL GTF or TIGRA VCF file.  If not specified reads from stdin
+# -o out.fn: TSV format output.  If not specified writes to stdout
+# -m mode: type of file, either "vcf" or "gtf".  If not specified then based on dat.fn extension
+
+
+# Based on /gscuser/mwyczalk/projects/Virus/Virus_2013.9a/analysis/UnifiedVirus2/PostBreakpointList/D_Ensembl/src/GTFExamine.R
+# Generalized to work with any such file. 
+
+suppressMessages(library(splitstackshape))
+suppressMessages(library("reshape2"))
+library(tools)
+
+options("width"=180) # useful for debugging
+
+get_val_arg = function(args, flag, default) {
+    ix = pmatch(flag, args)
+    if (!is.na(ix)){ val = args[ix+1] } else { val = default }
+    return(val)
+}
+
+get_bool_arg = function(args, flag) {
+    ix = pmatch(flag, args)
+    if (!is.na(ix)){ val = TRUE } else { val = FALSE }
+    return(val)
+}
+
+parse_args = function() {
+    args = commandArgs(trailingOnly = TRUE)
+
+    # optional arguments
+    verbose = get_bool_arg(args, "-v")
+    out.fn = get_val_arg(args, "-o", NULL)
+    dat.fn = get_val_arg(args, "-f", NULL)
+    file.type = get_val_arg(args, "-m", NULL)
+
+    # mandatory positional arguments.  These are popped off the back of the array, last one listed first.
+    # dat.fn = args[length(args)];               args = args[-length(args)]
+
+    val = list( 'verbose'=verbose, 'out.fn'=out.fn, 'dat.fn'=dat.fn, 'file.type'=file.type)
+    if (val$verbose) { print(val) }
+
+    return (val)
+}
+
+trim = function( x ) {
+  gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
+}
+
+# obtain and validate file format 
+# if file.type is passed use that, otherwise obtain file format from extension of data.fn
+# if format is unknown, or if data.fn extension is ambiguous, quit with an error
+detect.file.format = function(file.type, data.fn) {
+    if (is.null(file.type)) {
+        file.type = file_ext(data.fn)
+    }
+    if (!file.type %in% c("vcf", "gtf")) {
+        stop(paste("Unknown file type", file.type))
+    }
+    return(file.type)
+}
+
+cast.wide = function(data.split, file.format) {
+    if (file.format == 'gtf') {
+        data = dcast(data.split, seqname + source + feature + start + end + score + strand + frame + feature.id ~ key, value.var="value")
+    } else if (file.format == 'vcf') {
+        data = dcast(data.split, chrom + pos + id + ref + alt + qual + filter + format + spikein + V11 + feature.id ~ key, value.var="value")
+    }
+}
+
+args = parse_args()
+
+file.format = detect.file.format(args$file.type, args$dat.fn)
+
+if (file.format == "gtf") {
+    # col.names from ENSEMBL GTF specification, see D_Ensembl/README.ensembl
+    all.cols = c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute")
+    col.complex = "attribute"
+    field.delimit = ";"
+    key.value.delimit = " "
+} else if (file.format == "vcf") {
+    # VCF colnames from vafFilter.py
+    all.cols = c("chrom", "pos", "id", "ref", "alt", "qual", "filter", "info", "format", "spikein", "V11")
+    col.complex = "info"
+    field.delimit = ";"
+    key.value.delimit = "="
+}
+
+if (is.null(args$dat.fn)) {
+    fin = file('stdin')
+} else {
+    fin = args$dat.fn
+}
+
+data = read.table(fin, col.names=all.cols, sep="\t", row.names=NULL)
+data$feature.id = row.names(data)  # to ensure uniqueness when doing dcast
+
+# cSplit is the magic here
+# http://www.r-bloggers.com/splitstackshape-v1-4-0-for-r/
+# http://stackoverflow.com/questions/24595421/how-to-strsplit-data-frame-column-and-replicate-rows-accordingly
+data.split = as.data.frame(cSplit(data, col.complex, sep = field.delimit, direction="long"))
+
+
+key.value = as.data.frame(do.call(rbind, strsplit(trim(data.split[,col.complex]), key.value.delimit)))
+names(key.value) = c("key", "value")
+data.split = cbind(data.split, key.value)
+data.split[,col.complex] = NULL
+
+# http://stackoverflow.com/questions/14262741/combining-duplicated-rows-in-r-and-adding-new-column-containing-ids-of-duplicate
+# collapse duplicate attribute entries into one row, separated by semicolons.  An example where this is needed is in a GTF attribute
+# string like, 
+#   tag "cds_end_NF"; tag "mRNA_end_NF";
+data.split = aggregate(data.split, data.split[,c("feature.id", "key")], FUN = function(X) paste(unique(X), collapse=";"))
+# aggregate duplicates two columns, so we get rid of them.
+data.split = data.split[,3:ncol(data.split)]
+
+# Cast into wide format
+data = cast.wide(data.split, file.format)
+
+# remove the feature ID column because its for internal use only
+data$feature.id = NULL
+data = unique(data)
+
+if (is.null(args$out.fn)) {
+    out.fn = ""
+} else {
+    out.fn = args$out.fn
+}
+
+write.table(data, file=, sep="\t", quote=FALSE, row.names=FALSE)
+if (is.null(args$out.fn)) {
+    cat(sprintf("    Saved to %s\n", args$out.fn))
+}
