aa0bd381040f5aed030b0f91098762a5476516ec
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-05-06 15:37:20 -0500

    Adding SAMFilter, to retain reads according to BPR filter

--- src/util/SAMFilter.py
+++ src/util/SAMFilter.py
@@ -33,47 +33,52 @@ class BPR:
          Return (chromA, startA, endA, chromB, startB, endB, attr)
          If attribute not specified in file, attr=None """
         # Consider first seven columns, 
-        t = line.rstrip().split("\t")[0:6]
+        t = line.rstrip().split("\t")
         attr = None if len(t) == 6 else t[6]
+        padding = int(padding)
         return (t[0], int(t[1])-padding, int(t[2])+padding, t[3], int(t[4])-padding, int(t[5])+padding, attr)
 
     def contains(self, chromA, posA, chromB, posB):
         """Test whether chromA/posA and chromB/posB fall within any BPR region.  Assume A,B ordered in BPC format.  """
         # Note that if pos falls on a boundary it is considered within BPR
+        # print "Testing " + str( (chromA, posA, chromB, posB))
         for (bpr_cA, bpr_sA, bpr_eA, bpr_cB, bpr_sB, bpr_eB, a) in self.BPR:
-            inA = (chromA == bpr_cA) and (posA >= bpr_sA) and (posA <= bpr_sA)
-            inB = (chromB == bpr_cB) and (posB >= bpr_sB) and (posB <= bpr_sB)
+            # print "Against " + str( (bpr_cA, bpr_sA, bpr_eA, bpr_cB, bpr_sB, bpr_eB, a) )
+            inA = (chromA == bpr_cA) and (posA >= bpr_sA) and (posA <= bpr_eA)
+            inB = (chromB == bpr_cB) and (posB >= bpr_sB) and (posB <= bpr_eB)
+
             if inA and inB: return True
 
         return False
 
 
-########
-
-
 # See here for writing to/from stdin/out: http://pysam.readthedocs.io/en/latest/usage.html#using-streams
 
 
-# for utility purposes only...
-def filter_SAM(f, o, bpr, options) :
-    insam = pysam.AlignmentFile(f, "r")
-    if options.header:
-        outsam = pysam.AlignmentFile(o, "w", template=insam)
-    else:
-        outsam = pysam.AlignmentFile(o, "w")
-
+def filter_SAM(insam, outsam, bpr, options):
 
     #process all reads in region
     for read in insam.fetch():
         # We define position of read as first mapped position
 
+        chromA = insam.get_reference_name(read.reference_id)
         posA = read.get_reference_positions()[0]
-        chromA = read.get_reference_sequence()   # ???
-        posB = read.next_reference_start
         chromB = read.next_reference_name
+        posB = read.next_reference_start
 
-        if bmr.contains(chromA, posA, chromB, posB):
+        if bpr.contains(chromA, posA, chromB, posB):
             outsam.write(read)
+
+# Return input and output SAM AlignmentFile objects 
+def open_SAM(infn, outfn):
+
+    if infn == "stdin": infn = "-"
+    if outfn == "stdout": outfn = "-"
+
+    insam = pysam.AlignmentFile(infn, "r")
+    outsam = pysam.AlignmentFile(outfn, "w", template=insam)
+
+    return (insam, outsam)
 
 def main():
     from optparse import OptionParser
@@ -88,7 +93,6 @@ Filter reads to retain only those that fall within regions in BPR.dat
     parser.add_option("-i", dest="infn", default="stdin", help="Input SAM file to parse.  Default reads from stdin")
     parser.add_option("-o", dest="outfn", default="stdout", help="Output SAM file to write.  Default writes to stdout")
     parser.add_option("-p", dest="padding", default="0", help="Expand BPR regions by this amount")
-    parser.add_option("-H", dest="copy_header", action="store_true", help="Copy header from input SAM into output SAM")
 
     (options, params) = parser.parse_args()
 
@@ -96,22 +100,14 @@ Filter reads to retain only those that fall within regions in BPR.dat
         parser.error("Pass at least one argument.")
     bprfn = params[0]
 
-    # this will be modified to open an AlignmentFile, possibly with header.  See http://pysam.readthedocs.io/en/latest/api.html#sam-bam-files
-    if infn == "stdin":
-        f = sys.stdin
-    else:
-        f = open(infn, 'r')
-    if outfn == "stdout":
-        o = sys.stdout
-    else:
-        o = open(outfn, "w")
+    bpr = BPR(bprfn, options.padding)
 
-    BPR = BPR(bprfn, options.padding)
+    (insam, outsam) = open_SAM(options.infn, options.outfn)
 
-    filter_SAM(f, o, BPR, options)
+    filter_SAM(insam, outsam, bpr, options)
 
-    f.close()
-    o.close()
+    insam.close()
+    outsam.close()
 
 
 if __name__ == '__main__':
